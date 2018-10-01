6b933a953584e868daa839218622925b58ae4118
kristopherbullinger <github email address>
2018-10-01 14:08:13 -0400

    Lab Complete

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,16 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+puts "Would you like information on a character or movie?"
+input = gets.chomp
+if input.downcase == "character"
+  puts "Which character?"
+  character = get_character_from_user
+  show_character_movies(character)
+elsif input.downcase == "movie"
+  puts "What movie?"
+  movie = get_movie_from_user
+  show_movie_info(movie)
+else
+  "please enter a valid input next time"
+end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,13 @@
 def welcome
-  # puts out a welcome message here!
+  puts "HIIII"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "please enter a character's full name"
+  character = gets.chomp
+end
+
+def get_movie_from_user
+  puts "Please enter a movie title"
+  movie = gets.chomp
 end
